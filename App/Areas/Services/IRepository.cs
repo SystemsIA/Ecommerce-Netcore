ecbe1a4b18843875d7668090da72faa190a7ffe1
josetanta <jose.tanta.27@unsch.edu.pe>
2022-01-20 18:17:03 -0500

    Added Inteface-Repository

---
+++ App/Areas/Services/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace App.Areas.Services
+{
+	public interface IRepository<T>
+	{
+		public Task<T> GetById(int id);
+		public Task<bool> ExistObject(string name);
+		public Task<ICollection<T>> GetList();
+		public Task<T> Save(T obj);
+	}
+}
